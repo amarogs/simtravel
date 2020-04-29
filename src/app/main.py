e7ba385fe7ab5fbf76a5ab82a501527766db5c43
Amaro <amagarsua@alum.us.es>
2020-04-29 20:26:56 +0200

    deleting old analysis

--- src/app/main.py
+++ src/app/main.py
@@ -163,15 +163,3 @@ class SimtravelMainWindow(QMainWindow):
 
         return super().closeEvent(cls)
 
-if __name__ == "__main__":
-    # enable highdpi scaling
-    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)  
-    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
-
-    # Create the application 
-    app = QApplication([])
-    QtGui.QApplication.setStyle(QtGui.QStyleFactory.create('Cleanlooks'))
-    app.setApplicationName("SIMTRAVEL")
-    main_window = SimtravelMainWindow()
-    main_window.show()
-    app.exec_()
